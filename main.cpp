be6972409441551a93aca109ee8ea0e85701da25
Ahmed Mansour <133324agh@gmail.com>
2024-05-08 13:54:55 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+typedef std::string text_t;
+using num = int;
+using namespace std;
+
+int sq()
+{
+
+    double a;
+    double b;
+    double c;
+    cout << "add A\n";
+    cin >> a;
+    cout << "add B\n";
+    cin >> b;
+    c = sqrt(pow(a, 2) + pow(b, 2));
+    cout << "c is " << c;
+    return 0;
+}
+
+int main()
+{
+   //sq();
+    // const double n = (int)3.55;
+    // cout << "\t" << n << "\n";
+
+    // cout << "Hello\n";
+    // text_t Aname = "ahmed name \n";
+
+    // cout << Aname;
+    // string name = "ahmed\n \t";
+    // cout << name + "\n";
+
+    // // Take input using cin
+    // cin >> name;
+
+    // // Print output
+    // cout << " \n" + name + "";
+   
+   
+   
+    return 0;
+}
